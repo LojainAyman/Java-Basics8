8611ccbc780e002c0ee2706fad67d951c7362d9a
LojainAyman <lojainalmfleh@gmail.com>
2024-05-18 20:27:33 +0300

    update

---
+++ src/MyClass.java
@@ -0,0 +1,125 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+
+ //(problem solving)
+//		for (int i = 0; i <= 10; i++) {
+//			  System.out.println(i);
+//			}
+//----------------
+//		for (int i = 0; i <= 10; i=i+2) {
+//			  System.out.println(i);
+//			} // interview question
+//		
+		
+//---------------		
+//		for (int i = 50; i <= 100; i=i+2) {
+//			  System.out.println(i);
+//		} // interview question
+//		
+		
+//		for (int i = 51; i <= 100; i=i+2) {
+//			  System.out.println(i);
+//			} // interview question
+//		
+//------------------
+//		for (int i = 50; i < 100; i=i+2) {
+//			  System.out.println(i+1);
+//			} // interview question
+	
+//--------------		
+//		// assign
+//		String name = "lojain";
+//				
+//				
+//		for (int i = 50; i%2==0; i=i+2) {
+//			if(i==100) {
+//				break ;
+//			}
+//			  System.out.println(i);
+//			} // interview question
+//----------------------		
+//		// assign
+//		String name = "lojain";
+//				
+//				
+//		for (int i = 50; i%2==0; i=i+2) {
+//			 System.out.println(i);
+//			 
+//			if(i==100) {
+//				break ;
+//			}
+//			  
+//			} // interview question
+//		
+//--------------------		
+//		// assign
+//				String name = "lojain";
+//						
+//						
+//				for (int i = 50; i<100; i++) {
+//					
+//					if(i==50) {
+//						continue ;
+//					}
+//					System.out.println(i);
+//					  
+//					} // interview question (هون يعني 50 ماتنطبع
+//				
+//--------------------------		
+//		// assign
+//		String name = "lojain";
+//				
+//				
+//		for (int i = 50; i<80; i++) {
+//			
+//			if(i==50||i==60||i==70||i==80) {
+//				continue ;
+//			}
+//			System.out.println(i);
+//			  
+//			} // interview question  
+//------------------------
+		
+//		// assign
+//				String name = "lojain";
+//						
+//						
+//				for (int i = 50; i<=80; i++) {
+//					
+//					if(i==50&&i==60&&i==70&&i==80) {
+//						continue ;
+//					}
+//					System.out.println(i);
+//					  
+//					} // interview question  
+//-------------------	
+//		// assign
+//				String name = "lojain";
+//						
+//						
+//				for (int i = 50; i<=80; i++) {
+//					
+//					if(i==50&&i%2==0) {
+//						continue ;
+//					}
+//					System.out.println(i);
+//					  
+//					} // interview question  
+//---------------------
+	int myage = 29;
+	
+		while (myage==29) {	
+			System.out.println(myage);
+			myage++;
+		
+	}
+		System.out.println(myage);
+		//----------------------
+		
+	
+		
+
+	}		
+}
